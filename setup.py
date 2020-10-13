88cffcd266523068b985fb5d2c9ad7371b491a9f
Andreas Kvas <andreas@kvas.at>
2020-10-13 09:35:42 +0200

    added matplotlib dependency

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.1',
     author='Andreas Kvas',
     description='A python package to compute mass transport from satellite gravimetry',
-    install_requires=['numpy', 'scipy', 'netcdf4', 'numpydoc', 'cartopy'],
+    install_requires=['numpy', 'scipy', 'netcdf4', 'numpydoc', 'cartopy', 'matplotlib'],
     packages=['grates'],
     package_data={'grates': ['data/ddk_normals.npz', 'data/loadLoveNumbers_Gegout97.txt']}
 )
