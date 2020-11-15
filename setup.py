241f9b08e10ef352c63d82872ed97f789c05307b
Andreas Kvas <kvas@tugraz.at>
2020-11-15 09:53:48 +0100

    updated name of DDK filter data in setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     description='A python package to compute mass transport from satellite gravimetry',
     install_requires=['numpy', 'scipy', 'netcdf4', 'numpydoc', 'cartopy', 'matplotlib'],
     packages=['grates'],
-    package_data={'grates': ['data/ddk_normals.npz', 'data/loadLoveNumbers_Gegout97.txt']}
+    package_data={'grates': ['data/ddk_normal_blocks.npz', 'data/loadLoveNumbers_Gegout97.txt']}
 )
