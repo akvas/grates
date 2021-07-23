ec000b2a603d2b0b387151048728f09dd5f36634
Andreas Kvas <kvas@tugraz.at>
2021-07-23 09:14:24 +0200

    code cleanup

--- grates/grid.py
+++ grates/grid.py
@@ -1362,7 +1362,6 @@ class CSRMasconGridRL06(IrregularGrid):
     def __init__(self):
 
         lon, lat, area, self.__polygon_points, self.__point_to_vertex, self.__polygon_index, self.ocean_mask = grates.data.csr_rl06_mascon_grid()
-
         super(CSRMasconGridRL06, self).__init__(lon, lat, area, a=6378137.0, f=298.257223563**-1)
 
     def copy(self):
